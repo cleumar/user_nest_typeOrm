1adf5450d3f970e3c2cc77781b7ccbb04efabf0a
cleumar <cleumar.mendes@gmail.com>
2023-05-03 12:36:12 -0300

    feat: add multiplus upload file

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Body, Controller, Post, UploadedFile, UseGuards, UseInterceptors } from "@nestjs/common";
+import { BadRequestException, Body, Controller, Post, UploadedFile, UploadedFiles, UseGuards, UseInterceptors } from "@nestjs/common";
 
 
 import { AuthLoginDto } from "./dto/auth-login-dto";
@@ -9,7 +9,7 @@ import { AuthService } from "./auth.service";
 import { UserService } from "src/user/user.service";
 import { AuthGuard } from "src/guards/auth.guards";
 import { User } from "src/decorators/user.decorator";
-import { FileInterceptor } from "@nestjs/platform-express";
+import { FileFieldsInterceptor, FileInterceptor, FilesInterceptor } from "@nestjs/platform-express";
 import { join } from 'path'
 import { FileService } from "src/file/file.service";
 
@@ -64,6 +64,30 @@ constructor(
         }
          return {sucess: true}
     }
+
+    @UseInterceptors(FilesInterceptor('files'))
+    @UseGuards(AuthGuard)
+    @Post('files')
+    async uploadFiles (@User() user, @UploadedFiles()  files: Express.Multer.File[]) {    
+         return files
+    }
+
+    @UseInterceptors(FileFieldsInterceptor([
+        {
+        name: 'photo',
+        maxCount: 1
+        },
+        {
+            name:'documents',
+            maxCount: 10
+        }]
+    ))
+    @UseGuards(AuthGuard)
+    @Post('files-fields')
+    async uploadFilesFields (@User() user, @UploadedFiles()  files: {photo: Express.Multer.File, documents: Express.Multer.File[]}) {    
+         return {files}
+    }
+
 
     
 
